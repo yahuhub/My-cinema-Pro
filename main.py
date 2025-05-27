40628d8e0bceffa03c8b160c13a2e22e50f3ec39
yahuhub <119478670+yahuhub@users.noreply.github.com>
2025-05-27 14:31:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ WELCOME_MESSAGE =
 welcome_text = "Welcome to MY CINEMA PRO bot where you can search movies and get direct links privately."
 Your personal movie assistant is here!  
  welcome_text = "Search your favorite movies in the group, and I'll instantly deliver them to your private chat - safely and directly!"
-Note: Please don’t forget to start me first!
+note_text = "Please don't forget to start me first!"
 
 For any help, contact admin: @Dm_Anonymous  
 Enjoy your show!
